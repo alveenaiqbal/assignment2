590564f825d99b6b9e0c1211286a0e65de9afe75
alveena iqbal <alveenaiqbal993@gmail.com>
2025-04-10 21:38:42 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import ProductGrid from "./components/card";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <ProductGrid />
+    </div>
+  );
+}
+
+export default App;
+
